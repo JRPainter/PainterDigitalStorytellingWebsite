1841bf314f278b210c402c9bb590b3b7d8f90ae5
Joseph Painter <129447076+JRPainter@users.noreply.github.com>
2024-09-18 20:19:42 -0500

    update sketch.js
    Added the rects in the background
    Gradient behind background

--- sketch.js
+++ sketch.js
@@ -1,14 +1,17 @@
-let circleReady, mouseClicked, isWhite, isRed, isYellow, isGreen, isOrange, isBlue, isPurple;
-let circleX, circleY, circleHeight, circleWidth;
+let circleReady, isWhite, isRed, isYellow, isGreen, isOrange, isBlue, isPurple, xPolarity, yPolarity, colorReveal;
+let circleX, circleY, circleHeight, circleWidth, red1, red2, green1, green2, blue1, blue2, backgroundOpacity, clickCount;
+let circleColor, gradient1, gradient2;
+let movementSpeedX, movementSpeedY;
 
+let circles = [];
+let rectHeightClose = [];
+let rectHeightFar = [];
 
 
 function setup() {
     createCanvas(1000, 600);
     noCursor();
-    cursor(CROSS, 20, -10);
      circleReady = false;
-     mouseClicked = false;
      isWhite = false;
      isRed = false;
      isYellow = false;
@@ -16,10 +19,33 @@ function setup() {
      isOrange = false;
      isBlue = false;
      isPurple = false;
+     colorReveal = true;
      circleX = 0;
      circleY = 0;
      circleHeight = 50;
      circleWidth = 50;
+     red1 = 255;
+     red2 = 0;
+     green1 = 0;
+     green2 = 0;
+     blue1 = 0;
+     blue2 = 255;
+     backgroundOpacity = 255;
+     clickCount = 0;
+     circleColor = color(0,0,0);
+     gradient1 = color(255,0,0);
+     gradient2 = color(0,0,255);
+     movementSpeedX = random(5);
+     movementSpeedY = random(3);
+     xPolarity = Math.random() < 0.5;
+     yPolarity = Math.random() < 0.5;
+
+     for (let i = 0; i < 15; i++) {
+      rectHeightClose.push(random(125, 250));
+     }
+     for (let i = 0; i < 25; i++) {
+      rectHeightFar.push(random(40, 135));
+     }
 
     
   }
@@ -27,51 +53,80 @@ function setup() {
 
 
   function draw() {
+    
+    gradient1 = color(red1, green1, blue1);
+    gradient2 = color(red2, green2, blue2);
+    if (colorReveal) {
+      setGradient(gradient1, gradient2);
+      backgroundOpacity -= 0.25;
+    }
+    if (backgroundOpacity > 0) {
+      background(25,25,25,backgroundOpacity);      
+    }
 
-    background(220);
+    
+    let farCount = 0;
+    let closeCount = 0;
+    noStroke();
+
+    for (let j = 0; j < width; j += width/25) {
+      fill(75);
+      rect(j, rectHeightFar[farCount], width/25, height);
+      farCount++;
+    }
+
+    for (let k = 0; k < width; k += width/15) {
+      fill(125);
+      rect(k, rectHeightClose[closeCount], width/15, height);
+      closeCount++;
+    }
 
 
     //white box
     isWhite = true;
     fillWhite();
     isWhite = false;
-    rect(width*.05,height*.5,400,250);
+    strokeWeight(5);
+    stroke(200);
+    rect(width*.05,height*.5,width*0.4,width*0.25);
+    strokeWeight(2);
+    stroke(0);
 
     //red
     isRed = true;
     fillRed();
     isRed = false;
-    ellipse(width*.55,height*.8,100,100);
+    ellipse(width*.55,height*.8,width*0.1,width*0.1);
 
     //yellow
     isYellow = true;
     fillYellow();
     isYellow = false;
-    ellipse(width*.7,height*.8,100,100);
+    ellipse(width*0.7,height*0.8,width*0.1,width*0.1);
 
     //green
     isGreen = true;
     fillGreen();
     isGreen = false;
-    ellipse(width*.55,height*.6,100,100);
+    ellipse(width*.55,height*.6,width*0.1,width*0.1);
 
     //orange
     isOrange = true;
     fillOrange();
     isOrange = false; 
-    ellipse(width*.7,height*.6,100,100);
+    ellipse(width*.7,height*.6,width*0.1,width*0.1);
 
     //blue
     isBlue = true;
     fillBlue();
     isBlue = false;
-    ellipse(width*.85,height*.8,100,100);
+    ellipse(width*.85,height*.8,width*0.1,width*0.1);
 
     //purple
     isPurple = true;
     fillPurple();
     isPurple = false;
-    ellipse(width*.85,height*.6,100,100);
+    ellipse(width*.85,height*.6,width*0.1,width*0.1);
 
     paintBlue();
     paintPurple();
@@ -80,26 +135,66 @@ function setup() {
     paintRed();
     paintYellow();
 
-    
-    if (mouseIsPressed && isInBox()) {
-      circleReady = true;
-      circleX = mouseX;
-      circleY = mouseY;
-    }
+    randomSpeedX = random(5);
+    randomSpeedY = random(3);
 
-    if (circleReady == true) {
-      ellipse(circleX, circleY, circleWidth, circleHeight);
+    xPolarity = Math.random() < 0.5;
+    yPolarity = Math.random() < 0.5;
+
+    fill(circleColor);
+    strokeWeight(2);
+    stroke(50);
+    ellipse(mouseX, mouseY, circleWidth/2, circleHeight/2);
+
+    noStroke();
+    for (let i = 0; i < circles.length; i++) {
+      circles[i].display();
     }
-    
+    if (colorReveal) {
+    if(red1 == 255) {
+      green1++;
+      if(blue1 > 0) {
+        blue1--;
+      }
+    }
+    if(green1 == 255) {
+      blue1++;
+      if(red1 > 0) {
+        red1--;
+      }
+    }
+    if(blue1 == 255) {
+      red1++;
+      if(green1 > 0) {
+        green1--;
+      }
+    }
+    if(red2 == 255) {
+      green2++;
+      if(blue2 > 0) {
+        blue2--;
+      }
+    }
+    if(green2 == 255) {
+      blue2++;
+      if(red2 > 0) {
+        red2--;
+      }
+    }
+    if(blue2 == 255) {
+      red2++;
+      if(green2 > 0) {
+        green2--;
+      }
+    }
   }
 
 
 
-  function isMouseClicked() {
-    if (mouseIsPressed) {
-      mouseClicked = true;
-    }
+    
   }
+
+
 
 
   function createCircle() {
@@ -107,82 +202,48 @@ function setup() {
   }
 
   function isInBox() {
-    if(mouseX > width*.05 && mouseX < width*.45 && mouseY > height*.5 && mouseY < height*.75) {
+    if(mouseX > width*.05 && mouseX < width*.05 + width*0.4 && mouseY > height*.5 && mouseY < height*.5 + width*.25) {
       return true;
     }
     return false;
   }
 
   function paintBlue() {
-    if (mouseX > width*.8 && mouseX < width*.9 && mouseY > height*.75 && mouseY < height*.85) {
-      isBlue = true;
-      fillBlue();
-      isPurple = false;
-      isGreen = false;
-      isOrange = false;
-      isRed = false; 
-      isYellow = false;
+    if (mouseX > width*.8 && mouseX < width*.9 && mouseY > height*.8 - width * 0.05 && mouseY < height*.8 + width * 0.05 && mouseIsPressed) {
+      circleColor = color(0,171,231,200);
     }
   }
   function paintPurple() {
-    if (mouseX > width*.8 && mouseX < width*.9 && mouseY > height*.55 && mouseY < height*.65) {
-      isPurple = true;
-      fillPurple();
-      isBlue = false;
-      isGreen = false;
-      isOrange = false;
-      isRed = false;
-      isYellow = false;
+    if (mouseX > width*.8 && mouseX < width*.9 && mouseY > height*.6 - width * 0.05 && mouseY < height*.6 + width * 0.05 && mouseIsPressed) {
+      circleColor = color(115,25,95,200);
     }
   }
   function paintGreen() {
-    if (mouseX > width*.5 && mouseX < width*.6 && mouseY > height*.55 && mouseY < height*.65) {
-      isGreen = true;
-      fillGreen();
-      isBlue = false;
-      isPurple = false;
-      isOrange = false;
-      isRed = false;
-      isYellow = false;
+    if (mouseX > width*.5 && mouseX < width*.6 && mouseY > height*.6 - width * 0.05 && mouseY < height*.6 + width * 0.05 && mouseIsPressed) {
+      circleColor = color(22,244,208,200);
     }
   }
   function paintOrange() {
-    if (mouseX > width*.65 && mouseX < width*.75 && mouseY > height*.55 && mouseY < height*.65) {
-      isOrange = true;
-      fillOrange();
-      isBlue = false;
-      isPurple = false;
-      isGreen = false;
-      isRed = false;
-      isYellow = false;
+    if (mouseX > width*.65 && mouseX < width*.75 && mouseY > height*.6 - width * 0.05 && mouseY < height*.6 + width * 0.05 && mouseIsPressed) {
+      circleColor = color(255,125,0,200);
     }
   }
   function paintRed() {
-    if (mouseX > width*.5 && mouseX < width*.6 && mouseY > height*.75 && mouseY < height*.85) {
-      isRed = true;
-      fillRed();
-      isBlue = false;
-      isPurple = false;
-      isGreen = false;
-      isOrange = false;
-      isYellow = false;
+    if (mouseX > width*.5 && mouseX < width*.6 && mouseY > height*.8 - width * 0.05 && mouseY < height*.8 + width * 0.05 && mouseIsPressed) {
+      circleColor = color(237,37,78,200);
     }
   }
   function paintYellow() {
-    if (mouseX > width*.65 && mouseX < width*.75 && mouseY > height*.75 && mouseY < height*.85) {
-      isYellow = true;
-      fillYellow();
-      isBlue = false;
-      isPurple = false;
-      isGreen = false;
-      isOrange = false;
-      isRed = false;
+    if (mouseX > width*.65 && mouseX < width*.75 && mouseY > height*.8 - width * 0.05 && mouseY < height*.8 + width * 0.05 && mouseIsPressed) {
+      circleColor = color(249,220,92,200);
     }
   }
 
+  
+
   function fillBlue() {
     if (isBlue) {
-      fill(0,171,231);
+      fill(0,171,231);   
     }
   }
 
@@ -200,13 +261,13 @@ function setup() {
 
   function fillOrange() {
     if (isOrange) {
-      fill(255,125,0);
+      fill(255,125,0);  
     }
   } 
 
   function fillRed() {
     if (isRed) {
-      fill(237,37,78);
+      fill(237,37,78);    
     } 
   } 
 
@@ -219,5 +280,77 @@ function setup() {
   function fillWhite() {
     if (isWhite) {
       fill(255,255,255);
+    }
+  }
+
+  function mousePressed() {
+
+    let newCircle = new Circle(mouseX, mouseY, circleWidth, circleHeight, circleColor, randomSpeedX, randomSpeedY, xPolarity, yPolarity);
+    if(isInBox()) {
+      circles.push(newCircle);
+    } 
+    clickCount++;
+  }
+
+  function setGradient(c1, c2) {
+    noFill();
+    for (var y = 0; y < width; y++) {
+      var inter = map(y, 0, width, 0, 1);
+      var c = lerpColor(c1, c2, inter);
+      stroke(c);
+      //ellipse(width/2, y-100, width, y);
+      line(y, 0, y, height);
+    }
+  }
+
+  class Circle {
+
+    constructor(x, y, width, height, color, xSpeed, ySpeed, xDirection, yDirection) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.color = color;
+      this.xSpeed = xSpeed;
+      this.ySpeed = ySpeed;
+      this.xDirection = xDirection;
+      this.yDirection = yDirection;
+
+      if (this.xDirection) {
+        this.xSpeed = -this.xSpeed;
+      }
+      if (this.yDirection) {
+        this.ySpeed = -this.ySpeed;
+      }
+    }
+
+    display() {
+      fill(this.color);
+      ellipse(this.x, this.y, this.width, this.height);
+      this.x += this.xSpeed;
+      this.y += this.ySpeed;
+      if (this.x + this.width/2 > width*0.45) {
+        this.xSpeed = -this.xSpeed;
+        this.width -= 1;
+        this.height -= 1;
+      }
+      if (this.x - this.width/2 < width *0.05) {
+        this.xSpeed = -this.xSpeed;
+        this.width -= 1;
+        this.height -= 1;
+      }
+      if (this.y + this.height/2 > height*0.5 + width*0.25) {
+        this.ySpeed = -this.ySpeed;
+        this.width -= 1;
+        this.height -= 1;
+      }
+      if (this.y - this.height/2 < height*0.5) {
+        this.ySpeed = -this.ySpeed;
+        this.width -= 1;
+        this.height -= 1;
+      }
+      if (this.width == 0 && this.height == 0) {
+        circles.splice(circles.indexOf(this), 1);
+      }
     }
   }
