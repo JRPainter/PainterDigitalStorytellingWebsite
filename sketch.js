baf23b5885e89ea3d65e1a694472565366dc22a4
Joseph Painter <129447076+JRPainter@users.noreply.github.com>
2024-09-18 22:05:17 -0500

    Presentation Ready 9-18

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
-let circleReady, isWhite, isRed, isYellow, isGreen, isOrange, isBlue, isPurple, xPolarity, yPolarity, colorReveal;
-let circleX, circleY, circleHeight, circleWidth, red1, red2, green1, green2, blue1, blue2, backgroundOpacity, clickCount;
-let circleColor, gradient1, gradient2;
-let movementSpeedX, movementSpeedY;
+let circleReady, isWhite, isRed, isYellow, isGreen, isOrange, isBlue, isPurple, xPolarity, yPolarity, colorReveal, hasColor, redValue, greenValue, blueValue, orangeValue, yellowValue, purpleValue;
+let circleX, circleY, circleHeight, circleWidth, red1, red2, farRed, closeRed, green1, green2, farGreen, closeGreen, blue1, blue2, farBlue, closeBlue, backgroundOpacity, clickCount;
+let circleColor, gradient1, gradient2, farColor, closeColor;
+let movementSpeedX, movementSpeedY, movementSpeedHeight;
 
 let circles = [];
 let rectHeightClose = [];
 let rectHeightFar = [];
+let movementClose = [];
+let movementFar = [];
+
 
 
 function setup() {
@@ -19,33 +22,52 @@ function setup() {
      isOrange = false;
      isBlue = false;
      isPurple = false;
-     colorReveal = true;
+     colorReveal = false;
+     hasColor = false;
+     redValue = false;
+     greenValue = false;
+     blueValue = false;
+     orangeValue = false;
+     yellowValue = false;
+     purpleValue = false;
      circleX = 0;
      circleY = 0;
      circleHeight = 50;
      circleWidth = 50;
      red1 = 255;
      red2 = 0;
+     farRed = 75;
+     closeRed = 125;
      green1 = 0;
      green2 = 0;
+     farGreen = 75;
+     closeGreen = 125;
      blue1 = 0;
      blue2 = 255;
+     farBlue = 75;
+     closeBlue = 125;
      backgroundOpacity = 255;
      clickCount = 0;
      circleColor = color(0,0,0);
      gradient1 = color(255,0,0);
      gradient2 = color(0,0,255);
+     farColor = (75);
+     closeColor = (125);
      movementSpeedX = random(5);
      movementSpeedY = random(3);
+     movementSpeedHeight = 0;
      xPolarity = Math.random() < 0.5;
      yPolarity = Math.random() < 0.5;
 
      for (let i = 0; i < 15; i++) {
       rectHeightClose.push(random(125, 250));
+      movementClose.push(0);
      }
      for (let i = 0; i < 25; i++) {
       rectHeightFar.push(random(40, 135));
+      movementFar.push(0);
      }
+     
 
     
   }
@@ -58,7 +80,7 @@ function setup() {
     gradient2 = color(red2, green2, blue2);
     if (colorReveal) {
       setGradient(gradient1, gradient2);
-      backgroundOpacity -= 0.25;
+      backgroundOpacity -= 0.2;
     }
     if (backgroundOpacity > 0) {
       background(25,25,25,backgroundOpacity);      
@@ -69,16 +91,31 @@ function setup() {
     let closeCount = 0;
     noStroke();
 
+    closeColor = color(closeRed, closeGreen, closeBlue);
+    farColor = color(farRed, farGreen, farBlue);
+
     for (let j = 0; j < width; j += width/25) {
-      fill(75);
+      fill(farColor);
       rect(j, rectHeightFar[farCount], width/25, height);
+      rectHeightFar[farCount] += movementFar[farCount];
+      if (rectHeightFar[farCount] > 135 || rectHeightFar[farCount] < 40) {
+        movementFar[farCount] = -movementFar[farCount];
+      }
       farCount++;
     }
 
     for (let k = 0; k < width; k += width/15) {
-      fill(125);
+      fill(closeColor);
       rect(k, rectHeightClose[closeCount], width/15, height);
+      rectHeightClose[closeCount] += movementClose[closeCount];
+      if (rectHeightClose[closeCount] > 250 || rectHeightClose[closeCount] < 125) {
+        movementClose[closeCount] = -movementClose[closeCount];
+      }
       closeCount++;
+    }
+
+    if(clickCount > 125) {
+      colorReveal = true;
     }
 
 
@@ -146,6 +183,7 @@ function setup() {
     stroke(50);
     ellipse(mouseX, mouseY, circleWidth/2, circleHeight/2);
 
+
     noStroke();
     for (let i = 0; i < circles.length; i++) {
       circles[i].display();
@@ -189,7 +227,8 @@ function setup() {
     }
   }
 
-
+  console.log(farRed);
+  console.log(circleColor);
 
     
   }
@@ -211,31 +250,73 @@ function setup() {
   function paintBlue() {
     if (mouseX > width*.8 && mouseX < width*.9 && mouseY > height*.8 - width * 0.05 && mouseY < height*.8 + width * 0.05 && mouseIsPressed) {
       circleColor = color(0,171,231,200);
+      hasColor = true;
+      blueValue = true;
+      redValue = false;
+      greenValue = false;
+      orangeValue = false;
+      yellowValue = false;
+      purpleValue = false;      
     }
   }
   function paintPurple() {
     if (mouseX > width*.8 && mouseX < width*.9 && mouseY > height*.6 - width * 0.05 && mouseY < height*.6 + width * 0.05 && mouseIsPressed) {
       circleColor = color(115,25,95,200);
+      hasColor = true;
+      purpleValue = true;
+      redValue = false;
+      greenValue = false;
+      orangeValue = false;
+      yellowValue = false;
+      blueValue = false;
     }
   }
   function paintGreen() {
     if (mouseX > width*.5 && mouseX < width*.6 && mouseY > height*.6 - width * 0.05 && mouseY < height*.6 + width * 0.05 && mouseIsPressed) {
       circleColor = color(22,244,208,200);
+      hasColor = true;
+      greenValue = true;
+      redValue = false;
+      purpleValue = false;
+      orangeValue = false;
+      yellowValue = false;
+      blueValue = false;
     }
   }
   function paintOrange() {
     if (mouseX > width*.65 && mouseX < width*.75 && mouseY > height*.6 - width * 0.05 && mouseY < height*.6 + width * 0.05 && mouseIsPressed) {
       circleColor = color(255,125,0,200);
+      hasColor = true;
+      orangeValue = true;
+      redValue = false;
+      greenValue = false;
+      purpleValue = false;
+      yellowValue = false;
+      blueValue = false;
     }
   }
   function paintRed() {
     if (mouseX > width*.5 && mouseX < width*.6 && mouseY > height*.8 - width * 0.05 && mouseY < height*.8 + width * 0.05 && mouseIsPressed) {
       circleColor = color(237,37,78,200);
+      hasColor = true;
+      redValue = true;
+      greenValue = false;
+      purpleValue = false;
+      orangeValue = false;
+      yellowValue = false;
+      blueValue = false;
     }
   }
   function paintYellow() {
     if (mouseX > width*.65 && mouseX < width*.75 && mouseY > height*.8 - width * 0.05 && mouseY < height*.8 + width * 0.05 && mouseIsPressed) {
       circleColor = color(249,220,92,200);
+      hasColor = true;
+      yellowValue = true;
+      redValue = false;
+      greenValue = false;
+      purpleValue = false;
+      orangeValue = false;
+      blueValue = false;
     }
   }
 
@@ -286,10 +367,146 @@ function setup() {
   function mousePressed() {
 
     let newCircle = new Circle(mouseX, mouseY, circleWidth, circleHeight, circleColor, randomSpeedX, randomSpeedY, xPolarity, yPolarity);
-    if(isInBox()) {
+    if(isInBox() && hasColor) {
       circles.push(newCircle);
+      for (let i = 0; i < movementClose.length; i++) {
+        if (movementClose[i] < 40)
+          movementClose[i] += random(50)/2000;
+        }
+      for (let i = 0; i < movementFar.length; i++) {
+        if (movementFar[i] < 60)
+          movementFar[i] += random(50)/1000;
+        }
+
+      if (blueValue) {
+        if (farBlue < 200) {
+          farBlue++;
+        }
+        if (farRed > 0) {
+          farRed--;
+        }
+        if (farGreen > 0) {
+          farGreen--;
+        }
+        if (closeBlue < 255) {
+          closeBlue++;
+        }        
+        if (closeRed > 0) {
+          closeRed--;
+        }
+        if (closeGreen > 0) {
+          closeGreen--;
+       }
+      }
+      if (purpleValue) {
+        if (farRed < 200) {
+          farRed++;
+        }
+        if (farBlue < 200) {
+          farBlue++;
+        }
+        if (farGreen > 0) {
+          farGreen--;
+        }
+        if (closeRed < 255) {
+          closeRed++;
+        }        
+        if (closeBlue < 255) {
+          closeBlue++;
+        }
+        if (closeGreen > 0) {
+          closeGreen--;
+        }
+      }
+      if (greenValue) {
+        if (farGreen < 200) {
+          farGreen++;
+        }
+        if (farRed > 0) {
+          farRed--;
+        }
+        if (farBlue > 0) {
+          farBlue--;
+        }
+        if (closeGreen < 255) {
+          closeGreen++;
+        }        
+        if (closeRed > 0) {
+          closeRed--;
+        }
+        if (closeBlue > 0) {
+          closeBlue--;
+        }
+      }
+      if (orangeValue) {
+        if (farRed < 200) {
+          farRed++;
+        }
+        if (farGreen < 100) {
+          farGreen++;
+        }
+        if (farGreen > 100) {
+          farGreen--;
+        }
+        if (farBlue > 0) {
+          farBlue--;
+        }
+        if (closeRed < 255) {
+          closeRed++;
+        }        
+        if (closeGreen < 127) {
+          closeGreen++;
+        }
+        if (closeGreen > 127) {
+          closeGreen--;
+        }
+        if (closeBlue > 0) {
+          closeBlue--;
+        }
+      }
+      if (redValue) {
+        if (farRed < 200) {
+          farRed++;
+        }
+        if (farGreen > 0) {
+          farGreen--;
+        }
+        if (farBlue > 0) {
+          farBlue--;
+        }
+        if (closeRed < 255) {
+          closeRed++;
+        }        
+        if (closeGreen > 0) {
+          closeGreen--;
+        }
+        if (closeBlue > 0) {
+          closeBlue--;
+        }
+      }
+      if (yellowValue) {
+        if (farRed < 200) {
+          farRed++;
+        }
+        if (farGreen < 200) {
+          farGreen++;
+        }
+        if (farBlue > 0) {
+          farBlue--;
+        }
+        if (closeRed < 255) {
+          closeRed++;
+        }        
+        if (closeGreen < 255) {
+          closeGreen++;
+        }
+        if (closeBlue > 0) {
+          closeBlue--;
+        }
+      }
     } 
     clickCount++;
+    
   }
 
   function setGradient(c1, c2) {
